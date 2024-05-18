551ce78f9f4ece8ca6406bf371a3c110db86a9b0
Jacob Pfeifer <jacob@pfeifer.dev>
2024-05-18 16:32:16 -0400

    loading and saving comments

--- settings.go
+++ settings.go
@@ -19,16 +19,37 @@ func routeSettings(r *chi.Mux) {
 		check(err)
 	})
 
-	r.Post("/settings/{key}", func(w http.ResponseWriter, r *http.Request) {
-		key := chi.URLParam(r, "key")
-		value := r.FormValue("Value")
-		err := PutParam(ParamPath(key, false), []byte(value))
-		check(err)
+	r.Post("/settings", func(w http.ResponseWriter, r *http.Request) {
+    // get all form values
+    err := r.ParseForm()
+    check(err)
+    settings, err := GetSettings()
+    check(err)
+    for i, group := range settings {
+      for j, setting := range group.Settings {
+        value := r.FormValue(setting.Key)
+        settings[i].Settings[j].Param.Value = value
+        if settings[i].Settings[j].Type == "toggle" && value == "" {
+          settings[i].Settings[j].Param.Value = "0"
+        }
+      }
+    }
+    err = putSettingsParams(settings)
+    check(err)
 	})
+
+  // Set default settings
+  settings, err := GetSettings()
+  check(err)
+  err = putSettingsParams(settings)
+  check(err)
 }
 
 func GetSettings() ([]components.SettingGroup, error) {
 	exists, err := Exists("/data/openpilot")
+  if err != nil {
+    return nil, err
+  }
 	settingsPath := "./settings.json"
 	if exists {
 		settingsPath = "/data/openpilot/settings.json"
@@ -41,5 +62,34 @@ func GetSettings() ([]components.SettingGroup, error) {
   }
   err = json.Unmarshal(settingsData, &settings)
 
+  settings = getSettingsCurrentState(settings)
+
 	return settings, err
 }
+
+func getSettingsCurrentState(settings []components.SettingGroup) []components.SettingGroup {
+  for i, group := range settings {
+    for j, setting := range group.Settings {
+      value, err := GetParam(ParamPath(setting.Key, false))
+      if err != nil {
+        settings[i].Settings[j].Param.Value = settings[i].Settings[j].DefaultValue
+      } else {
+        settings[i].Settings[j].Param.Value = string(value)
+      }
+    }
+  }
+  return settings
+}
+
+func putSettingsParams(settings []components.SettingGroup) error {
+  for _, group := range settings {
+    for _, setting := range group.Settings {
+      err := PutParam(ParamPath(setting.Key, false), []byte(setting.Param.Value))
+      if err != nil {
+        return err
+      }
+    }
+  }
+  return nil
+}
+
